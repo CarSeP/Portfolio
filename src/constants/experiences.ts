d48ce994539f535090791ac369c304fe0a86aea3
CarSeP <vjpd64@gmail.com>
2025-11-12 21:57:42 -0400

    Implement experience component with dynamic data and description

---
+++ src/constants/experiences.ts
@@ -0,0 +1,20 @@
+export const experiences = [
+  {
+    title: "Lorem ipsum dolor sit amet",
+    date: "(2000 - 2001)",
+    description: [
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse soluta iusto provident doloremque maxime aliquid, animi facere reprehenderit minima optio quibusdam magnam quis omnis hic vel sequi voluptatem atque harum",
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse soluta iusto provident doloremque maxime aliquid, animi facere reprehenderit minima optio quibusdam magnam quis omnis hic vel sequi voluptatem atque harum",
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse soluta iusto provident doloremque maxime aliquid, animi facere reprehenderit minima optio quibusdam magnam quis omnis hic vel sequi voluptatem atque harum",
+    ],
+  },
+  {
+    title: "Lorem ipsum dolor sit amet",
+    date: "(2000 - 2001)",
+    description: [
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse soluta iusto provident doloremque maxime aliquid, animi facere reprehenderit minima optio quibusdam magnam quis omnis hic vel sequi voluptatem atque harum",
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse soluta iusto provident doloremque maxime aliquid, animi facere reprehenderit minima optio quibusdam magnam quis omnis hic vel sequi voluptatem atque harum",
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse soluta iusto provident doloremque maxime aliquid, animi facere reprehenderit minima optio quibusdam magnam quis omnis hic vel sequi voluptatem atque harum",
+    ],
+  },
+];
