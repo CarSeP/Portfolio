7f7b5d94cf785efa1df482e82d5ae789af5b04c6
CarSeP <vjpd64@gmail.com>
2025-11-14 20:57:06 -0400

    Implement Contact component with email input and tooltip functionality; add info constants for email and URLs

--- src/constants/info.ts
+++ src/constants/info.ts
@@ -2,9 +2,9 @@ export const info = {
   name: "Lorem ipsum dolor",
   description:
     "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur facere blanditiis dolore quasi.",
-  mail: "",
+  mail: "asdas@sadsadas.com",
   urls: {
-    github: "",
-    linkedin: "",
+    github: "http://www.google.com",
+    linkedin: "http://www.google.com",
   },
 };
