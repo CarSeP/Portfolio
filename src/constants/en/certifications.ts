eebe76d5fc16bf660960f75fcdeaa1fb851cea16
CarSeP <vjpd64@gmail.com>
2026-03-28 12:11:33 -0400

    feat: Add a certifications view

---
+++ src/constants/en/certifications.ts
@@ -0,0 +1,6 @@
+export const certifications = [
+  {
+    title: "Oral and Written Expression Techniques in Spanish",
+    img: "/certifications/01.webp",
+  }
+];
---
+++ src/constants/es/certifications.ts
@@ -0,0 +1,6 @@
+export const certifications = [
+  {
+    title: "Técnicas de Expresión Oral y Escrita en Español",
+    img: "/certifications/01.webp",
+  }
+];
