82565316fc210a9a208643ebabb29c45fe1f975d
CarSeP <vjpd64@gmail.com>
2026-03-29 12:02:25 -0400

    feat: add english A2 certification

--- src/constants/en/certifications.ts
+++ src/constants/en/certifications.ts
@@ -1,6 +1,10 @@
 export const certifications = [
   {
+    title: "A2 English For Developers",
+    img: "/certifications/02.webp",
+  },
+  {
     title: "Oral and Written Expression Techniques in Spanish",
     img: "/certifications/01.webp",
-  }
+  },
 ];
--- src/constants/es/certifications.ts
+++ src/constants/es/certifications.ts
@@ -1,6 +1,10 @@
 export const certifications = [
   {
+    title: "Inglés A2 para desarrolladores",
+    img: "/certifications/02.webp",
+  },
+  {
     title: "Técnicas de Expresión Oral y Escrita en Español",
     img: "/certifications/01.webp",
-  }
+  },
 ];
