940363e63f9d608c5e20a5ba35af0634f7400304
CarSeP <vjpd64@gmail.com>
2026-06-12 15:57:27 -0400

    feat: add new certifications with images for JavaScript, Responsive Web Design, SQL, and Docker

--- src/constants/en/certifications.ts
+++ src/constants/en/certifications.ts
@@ -1,5 +1,21 @@
 export const certifications = [
   {
+    title: "JavaScript Certification",
+    img: "/certifications/05.webp",
+  },
+  {
+    title: "Responsive Web Design Certification",
+    img: "/certifications/06.webp",
+  },
+  {
+    title: "Introduction to Relational Databases in SQL",
+    img: "/certifications/04.webp",
+  },
+  {
+    title: "Introduction to Docker",
+    img: "/certifications/03.webp",
+  },
+  {
     title: "A2 English For Developers",
     img: "/certifications/02.webp",
   },
--- src/constants/es/certifications.ts
+++ src/constants/es/certifications.ts
@@ -1,5 +1,21 @@
 export const certifications = [
   {
+    title: "Certificación JavaScript",
+    img: "/certifications/05.webp",
+  },
+  {
+    title: "Certificación de Diseño Web Responsivo",
+    img: "/certifications/06.webp",
+  },
+  {
+    title: "Introducción a las bases de datos relacionales en SQL",
+    img: "/certifications/04.webp",
+  },
+  {
+    title: "Introducción a Docker",
+    img: "/certifications/03.webp",
+  },
+  {
     title: "Inglés A2 para desarrolladores",
     img: "/certifications/02.webp",
   },
